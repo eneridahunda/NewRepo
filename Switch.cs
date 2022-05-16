89d59145fcb5c23469f49e0f916bd8c3ede3136b
eneridahunda <105112492+eneridahunda@users.noreply.github.com>
2022-05-16 18:00:59 +0200

    Add files via upload

---
+++ Switch.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Example
+{ 
+   public static void Main()
+    {
+        Console.WriteLine("Input your number:");
+       
+        int nr = 0;
+      
+        switch (nr)
+
+       { 
+            case 0:
+            Console.ReadLine();
+            if (nr == 0)
+            Console.WriteLine("Correct");
+            Environment.Exit(0);
+            break;
+             
+           default:
+           Console.ReadLine();
+           if (nr != 0)
+           Console.WriteLine("Invalid input!");
+           Environment.Exit(0);
+           break;
+        }
+    }
+
+}
+    
+
+
